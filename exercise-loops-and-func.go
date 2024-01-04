d9b7dfc437676838e78ad563feca160fbcb7f9b5
linh <lxdntg@gmail.com>
2024-01-04 10:37:11 +0700

    exercise-loops-and-func

--- exercise-loops-and-func.go
+++ exercise-loops-and-func.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
 	z := float64(1)
-	for i := 1; i < 10; i++ {
+	for i := 1; i < 100; i++ {
+		oldZ := z
 		z -= (z*z - x) / (2 * z)
-		fmt.Println(z)
+		delta := math.Abs(z - oldZ)
+		fmt.Printf("Iteration %d: z = %v\n", i, z)
+		if delta < 1e-10 {
+			break
+		}
 	}
 	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	x := 3.0
+	sqrt := Sqrt(x)
+	fmt.Printf("Sqrt(%v) = %v\n", x, sqrt)
+	fmt.Printf("math.Sqrt(%v) = %v\n", x, math.Sqrt(x))
+	fmt.Printf("Delta(%v) = %v\n", x, sqrt-math.Sqrt(x))
 }

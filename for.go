2810eeae1a43a9211a26e7a430653b60d9fe109f
linh <lxdntg@gmail.com>
2024-01-04 09:52:57 +0700

    for

---
+++ for.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	//for continued, For is Go's "while"
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+
+}

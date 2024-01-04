0b937c8c721be70e5435b2d504f4891f5f7439e3
linh <lxdntg@gmail.com>
2024-01-05 00:47:24 +0700

    map

---
+++ maps.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type Vertex1 struct {
+	Lat, Long float64
+}
+
+var m map[string]Vertex1
+
+// Map literals
+var m2 = map[string]Vertex1{
+	"Bell Labs": Vertex1{
+		40.68433, -74.39967,
+	},
+	"Google": Vertex1{
+		37.42202, -122.08408,
+	},
+}
+
+//Map literals continued
+var m3 = map[string]Vertex1{
+	"Bell Labs": {40.68433, -74.39967},
+	"Google":    {37.42202, -122.08408},
+}
+
+func main() {
+	m = make(map[string]Vertex1)
+	m["Bell Labs"] = Vertex1{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
+
+	fmt.Println(m2)
+
+	fmt.Println(m3)
+
+	m4 := make(map[string]int)
+
+	m4["Answer"] = 42
+	fmt.Println("The value:", m4["Answer"])
+
+	m4["Answer"] = 48
+	fmt.Println("The value:", m4["Answer"])
+
+	delete(m4, "Answer")
+	fmt.Println("The value:", m4["Answer"])
+
+	v, ok := m4["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+}
